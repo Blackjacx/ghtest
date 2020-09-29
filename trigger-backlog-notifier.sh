db0756d0056379444437bf2134c196fcb7cdd765
Stefan Herold <stefan.herold@gmail.com>
2020-09-29 13:57:37 +0200

    Temporarily switch to feature branch

--- trigger-backlog-notifier.sh
+++ trigger-backlog-notifier.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
-tag="0.0.1" && git push --delete origin $tag && git tag -d $tag && git tag -a -m "My first action release" $tag && git push --tags
+tag="0.0.1" 
+git push --delete origin $tag 
+git tag -d $tag 
+git tag -a -m "Backlog Notifier Test Release" $tag 
+git push --tags
